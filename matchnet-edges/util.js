e009c0d66d8416917c51a478661ad4a1a5478758
Simon Jacobs <sdjacobs@uchicago.edu>
2015-08-17 09:39:13 -0500

    new matchnet init commit

---
+++ matchnet-edges/util.js
@@ -0,0 +1,74 @@
+
+function nodesFromLinks(links) {
+  
+  var nodesById = d3.map()
+  
+  function makeNode(s) {
+    var d = nodesById.get(s)
+    if (!d) {
+      d = {"name": s, sourceCount: 0, targetCount: 0, degree: 0}
+      nodesById.set(s, d)
+    }
+    return d;
+  }
+  for (d of links) {
+    d.source = makeNode(d.source)
+    d.target = makeNode(d.target)
+  
+    d.source.sourceCount = d.source.sourceCount + 1
+    d.target.targetCount = d.target.targetCount + 1
+    d.source.degree += 1
+    d.target.degree += 1
+  
+  }
+  return nodesById.values()
+}
+
+function getComponents(nodes, links)
+{
+  for (d of nodes) {
+    d.neighbors = []
+  }
+  for (d of links) {
+    d.target.neighbors.push(d.source)
+    d.source.neighbors.push(d.target)
+  }
+  
+  var components = []
+  
+  for (u of nodes) {
+    if (!u.visited) {
+      var c = get_component_dfs(u)
+      if (c.length > 0) {
+        var cs = d3.map(c, function(d) { return d.name })
+        var cl = links.filter(function(d) { return cs.has(d.source.name)})
+        components.push({nodes: c, links: cl})
+      }
+    }
+  }
+  return components
+}
+
+function get_component_dfs(u) {
+  
+  var component = d3.map()
+  function dfs(u) {
+    u.visited = true;
+    component.set(u.name, u)
+    for (v of u.neighbors) {
+      if (!v.visited) {
+        dfs(v)
+      }
+    }
+  }
+  dfs(u);
+  return component.values()
+}
+
+function getComponentGraph(nodesl, links) {
+  var nodes = d3.map(nodesl, function(d) { return d.name })
+  var links = links.filter(function(d) { return nodes.has(d.source.name)})
+  return {nodes:nodesl, links:links}
+}
+
+
